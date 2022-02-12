beaa782f06729b68938f9ab35109a653ff5ebced
Sushil Kumar <sushilkr820@gmail.com>
2022-02-12 15:51:59 +0530

    Hashing password using bcryptjs

--- cloudbook-backend/routes/auth.js
+++ cloudbook-backend/routes/auth.js
@@ -2,7 +2,10 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
+const JWT_SECRET = "SECRETCode";
 
 // Create a User using: POST "/api/auth/createuser". No login required for this 
 
@@ -20,20 +23,30 @@ router.post('/createuser', [
     // Check whether the user with this email exist or not 
 
     try {
-        
-        let user = await User.findOne({email: req.body.email});
-    
-        if(user) {
-            return res.status(400).json({error: "Sorry a user with email already exists"})
+        let user = await User.findOne({ email: req.body.email });
+
+        if (user) {
+            return res.status(400).json({ error: "Sorry a user with email already exists" })
         }
-    
-         user = await User.create({
+
+        const salt = bcrypt.genSaltSync(10);
+        const secretPass = await bcrypt.hash(req.body.password, salt);
+
+        // Create a new user
+        user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
-        })
-    
-        res.json(user)
+            password: secretPass,
+        });
+
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET);
+
+        res.json({authToken})
 
     } catch (error) {
         console.error(error.message);
