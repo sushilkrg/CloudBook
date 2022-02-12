efe25dbf86daec983e1a0083db59d3cac68498d1
Sushil Kumar <sushilkr820@gmail.com>
2022-02-12 15:09:58 +0530

    Modified some code related to createuser endpoint and Made a new POST request in Thunderclient

--- cloudbook-backend/routes/auth.js
+++ cloudbook-backend/routes/auth.js
@@ -4,26 +4,41 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
 
-// Create a User using: POST "/api/auth/" 
+// Create a User using: POST "/api/auth/createuser". No login required for this 
 
-router.post('/', [
+router.post('/createuser', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be atleast 5 characters long').isLength({ min: 5 })
-], (req, res) => {
+], async (req, res) => {
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
 
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    }).then(user => res.json(user))
-    .catch(err => {console.log(err)
-    res.json({error: 'Please enter unique email', message: err.message})});
+    // Check whether the user with this email exist or not 
+
+    try {
+        
+        let user = await User.findOne({email: req.body.email});
+    
+        if(user) {
+            return res.status(400).json({error: "Sorry a user with email already exists"})
+        }
+    
+         user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+    
+        res.json(user)
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some error occured");
+    }
 
 })
 
