3929339bbb1b456670e386512988686257e3132d
Sushil Kumar <sushilkr820@gmail.com>
2022-02-12 21:00:50 +0530

    Created an endpoint getuser and a middleware to decode user from a jwt

--- cloudbook-backend/routes/auth.js
+++ cloudbook-backend/routes/auth.js
@@ -4,10 +4,11 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
+var fetchuser = require('../middleware/fetchuser')
 
 const JWT_SECRET = "SECRETCode";
 
-// Create a User using: POST "/api/auth/createuser". No login required for this 
+// Route 1 : Create a User using: POST "/api/auth/createuser". No login required for this 
 
 router.post('/createuser', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
@@ -53,7 +54,7 @@ router.post('/createuser', [
     }
 })
 
-// Authenticate a User using: POST "/api/auth/login". No login required for this 
+// Route 2 : Authenticate a User using: POST "/api/auth/login". No login required for this 
 router.post('/login', [
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password cannot be blank').exists()
@@ -90,4 +91,20 @@ router.post('/login', [
         res.status(500).send("Internal server error");
     }
 })
+
+// Route 3 : Authenticate a User using: POST "/api/auth/getuser". login required 
+
+router.post('/getuser', fetchuser, async (req, res) => {
+
+    try {
+        const userId = req.user.id;
+        const user = await User.findById(userId).select("-password");
+        res.send(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error");
+    }
+})
+
+
 module.exports = router
