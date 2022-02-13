91f6b00cb36ae9f1188a0cfaa127f62b80cd6b1d
Sushil Kumar <sushilkr820@gmail.com>
2022-02-13 15:50:20 +0530

    Created endpoint to update a note

--- cloudbook-backend/routes/notes.js
+++ cloudbook-backend/routes/notes.js
@@ -45,4 +45,41 @@ router.post('/addnote', fetchuser, [
         }
 })
 
+// Rouite 3: Update an existing Note using: PUT "/api/notes/updatenote/:id" Login required
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+
+        try {
+                const { title, description, tag } = req.body;
+
+                // Create a newNote object
+                const newNote = {};
+                if(title){
+                        newNote.title = title
+                }
+                if(description){
+                        newNote.description = description
+                }
+                if(tag){
+                        newNote.tag = tag
+                }
+
+                // Find the note to be updated and update it
+                let note = await Note.findById(req.params.id);
+                if(!note){
+                        return req.status(404).send("Not found")
+                }
+
+                if(note.user.toString() !== req.user.id){
+                        return res.status(401).send("Not allowed");
+                }
+
+                note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true});
+                res.json({note});
+
+        } catch (error) {
+                console.error(error.message);
+                res.status(500).send("Internal server error");
+        }
+})
+
 module.exports = router
